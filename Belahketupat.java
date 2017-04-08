05fc5200fb82bbf5f576e1137968b70c0f2be693
Daniel Gentha Ivan Desantha <project.xofrion@gmail.com>
2017-04-08 12:06:02 +0700

    Update Belahketupat.java

--- Belahketupat.java
+++ Belahketupat.java
@@ -25,10 +25,13 @@ public class Belahketupat {
         for(int x=n; x>=1; x--)
         {
             for(int y=x-1; y>=1; y--){
-                System.out.print(" ");
+                System.out.print("0");
             }
             for(int z=1; z<=((n+1)-x)*2; z++){
                 System.out.print("*");
+            }
+            for(int y=x-1; y>=1; y--){
+                System.out.print("0");
             }
         System.out.println();
         }
@@ -36,10 +39,13 @@ public class Belahketupat {
         for(int i=n; i>=1; i--)
         {
             for(int k=1; k<=n-i; k++){
-                System.out.print(" ");
+                System.out.print("0");
             }
             for(int j=i*2; j>=1; j--){
                 System.out.print("*");
+            }
+            for(int k=1; k<=n-i; k++){
+                System.out.print("0");
             }
             System.out.println();
         }
